60d0568b972d62d8f5f638d1850e382ee2a088bc
Edris Berg <ali_berg1@yahoo.com>
2021-11-16 09:20:04 -0800

    files setup

---
+++ deleteItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+// it doesn't like the delete name so I changed it to deleteItem
+export default class deleteItem extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
---
+++ fakeStore.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class fakeStore extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
