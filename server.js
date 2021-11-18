67b5042f90ad467125c2416abe8a941b0830285b
Ben Mills <fuzzyllama@gmail.com>
2021-11-17 16:40:44 -0800

    Sets up auth0

--- server.js
+++ server.js
@@ -12,14 +12,11 @@ mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology:
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('Mongoose is connected'));
-const verifyUser = require('./auth.js');
-
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 
 app.get('/', getProduct);
 // app.get('/product', getProduct);
@@ -27,10 +24,5 @@ app.get('/users');
 app.post('/products', handlePostProduct);
 app.delete('/products/:id', handleDeleteProduct);
 app.put('/products/:id', handlePutProduct);
-
-
-
-
-
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
