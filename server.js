23bb230f329f75175a7f960e26223bfe5c65cb79
Ben Mills <fuzzyllama@gmail.com>
2021-11-17 22:31:42 -0800

    Sets ups post and get users with auth0

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const handleDeleteProduct = require('./deleteItem.js');
 const handlePostProduct = require('./create.js');
 const handlePutProduct = require('./putItem.js');
+const handleGetUser = require('./getUsers.js');
+const handlePostUser = require('./postUsers.js');
 const getProduct = require('./getProducts.js')
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
@@ -19,8 +21,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', getProduct);
-// app.get('/product', getProduct);
-app.get('/users');
+app.get('/users', handleGetUser);
+app.post('/users', handlePostUser);
 app.post('/products', handlePostProduct);
 app.delete('/products/:id', handleDeleteProduct);
 app.put('/products/:id', handlePutProduct);
